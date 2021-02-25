864d9168b1ec029934398ffd62be10d313900ead
Gabby <gabegabby99@gmail.com>
2021-02-25 17:22:56 -0500

    Initial Commit

---
+++ AnimeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeApp
+//
+//  Created by Gabby Gonzalez on 2/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
