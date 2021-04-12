aba18c0d734a5a877bbd32d25bcb04aee0472e96
Gabby <gabegabby99@gmail.com>
2021-04-12 19:41:20 -0400

    fixed delete function

--- AnimeApp/AnimeCell.swift
+++ AnimeApp/AnimeCell.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class AnimeCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var sumLabel: UILabel!
     @IBOutlet weak var posterView: UIImageView!
+    
+    
+//    var anime: Anime! {
+//        titleLabel.text = anime.title
+//        sumLabel.text = anime.summary
+//        posterView.af.setImage(withURL: URL(anime.posterURL!))
+//        
+//    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
